96871f297df765b8d57632e4cee6c9c639331067
Alex Chescheir <alexchescheir@Nancys-MacBook-Pro.local>
2016-05-13 13:47:59 -0400

    Added model for TaskItems

--- kanban/tasklist/models.py
+++ kanban/tasklist/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class TaskItem(models.Model):
+    title = models.CharField(max_length=255)
+    BACKLOG = 'BL'
+    READY = 'RD'
+    DOING = 'DO'
+    DONE = 'DN'
+    STATUS_CHOICES = (
+        (BACKLOG, 'Backlog'),
+        (READY, 'Ready'),
+        (DOING, 'Doing'),
+        (DONE, 'Done'),
+        )
+    status = models.CharField(max_length=2,
+        choices=STATUS_CHOICES,
+        default=BACKLOG,
+        )
+
+    @staticmethod
+    def get_next_priority():
+        items = TaskItem.objects.order_by('-priority')
+        if len(items) == 0:
+            return 1
+        else
+            return items[0] + 1
+
+    priority = models.IntergerField(unique=True, default=TaskItem.get_next_priority())
+
+    def __str__(self):
+        return "{}: {}-{}".format([self.title,STATUS_CHOICES[self.status], self.priority])
+
+    
